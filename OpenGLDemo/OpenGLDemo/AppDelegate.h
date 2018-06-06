bdf56f23ca4d2aabf8553e525d4495fa8a5f1a52
gaochongyang <gaochongyang@enmonster.com>
2018-06-06 09:35:00 +0800

    创建工程

---
+++ OpenGLDemo/OpenGLDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  OpenGLDemo
+//
+//  Created by gaochongyang on 2018/6/6.
+//  Copyright © 2018年 gaochongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ OpenGLDemo/OpenGLDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OpenGLDemo
+//
+//  Created by gaochongyang on 2018/6/6.
+//  Copyright © 2018年 gaochongyang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
